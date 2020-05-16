4e1b119fd0ffd5553f0c68b252d8e0a3d13018c6
wertrain <wertrain@gmail.com>
2020-05-16 18:17:32 +0900

    プロジェクトの作成

---
+++ TinyUnzipper/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO.Compression;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace TinyUnzipper
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var app = new CommandLineApplication(throwOnUnexpectedArg: false);
+
+            app.Name = nameof(TinyUnzipper);
+            app.Description = "unzip the zip file.";
+            app.HelpOption("-h|--help");
+
+            var fileNameOption = app.Option(
+                template: "-f|--file",
+                description: "file name after unzip",
+                optionType: CommandOptionType.SingleValue
+            );
+
+            var filePathArgument = app.Argument(
+                name: "file",
+                description: "zip file path",
+                multipleValues: false);
+
+            app.OnExecute(() =>
+            {
+                try
+                {
+                    var filePath = filePathArgument.Value;
+                    if (filePath == null)
+                    {
+                        app.ShowHelp();
+                        return 1;
+                    }
+
+                    if (!System.IO.File.Exists(filePath))
+                    {
+                        return 1;
+                    }
+
+                    var unzipFileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
+                    if (fileNameOption.HasValue())
+                    {
+                        unzipFileName = fileNameOption.Value();
+                    }
+
+                    ZipFile.ExtractToDirectory(filePath, unzipFileName);
+                }
+                catch (System.Exception e)
+                {
+                    System.Console.WriteLine(e);
+                    return 2;
+                }
+                return 0;
+            });
+
+            return app.Execute(args);
+        }
+    }
+}
